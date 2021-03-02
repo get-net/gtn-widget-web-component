97345a2523c7cf0bff5023569d1a56333af360da
arsenitem <atemnik@get-net.ru>
2021-03-02 17:31:26 +0500

    form validation

--- src/components/constructor/ConstructorView.vue
+++ src/components/constructor/ConstructorView.vue
@@ -4,11 +4,12 @@
             <ncform
                 :form-schema="item"
                 :form-name="item.id"
+                v-model="item.value"
             ></ncform>
         </b-card>
         <b-card>
             <b-row>
-                <b-button>
+                <b-button @click="onSendFormClick">
                     Отправить
                 </b-button>
             </b-row>
@@ -19,9 +20,22 @@
 <script>
 export default {
     props: ['template'],
+
+    methods: {
+        onSendFormClick() {
+            this.template.forEach(item => {
+                this.$ncformValidate(item.id).then(data => {
+                if (data.result) {
+                    console.log(item.value)
+                    // do what you like to do
+                    }
+                })
+            })
+        }
+    }
 }
 </script>
-
+   
 <style>
 
 </style>
